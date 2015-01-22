3f9d495732b2be1eadb22ac651749b65211e2818
uhef <tuomas.jarvensivu@iki.fi>
2015-01-22 20:22:26 +0200

    Add in-application routing bundle

---
+++ bundles/routing/bundle/inapprouting/instance.js
@@ -0,0 +1,44 @@
+/**
+ * This bundle logs the map click coordinates to the console. This is a demonstration of using DefaultExtension.
+ *
+ * @class Oskari.routing.bundle.inapprouting.InAppRoutingBundleInstance
+ */
+Oskari.clazz.define('Oskari.routing.bundle.inapprouting.InAppRoutingBundleInstance',
+  /**
+   * @method create called automatically on construction
+   * @static
+   */
+  function () {
+    // Best practice is to initialize instance variables here.
+    this.myVar = undefined;
+  }, {
+    /**
+     * @static
+     * @property __name
+     */
+    __name : 'inapprouting',
+    /**
+     * Module protocol method
+     *
+     * @method getName
+     */
+    getName : function () {
+      return this.__name;
+    },
+    eventHandlers: {
+      'MapClickedEvent': function (event) {
+        console.log('Map clicked at', event.getLonLat());
+      }
+    },
+    /**
+     * DefaultExtension method for doing stuff after the bundle has started.
+     *
+     * @method afterStart
+     */
+    afterStart: function (sandbox) {
+      console.log('Bundle', this.getName(), 'started');
+      this.myVar = 'foobar';
+    }
+  }, {
+    "extend" : ["Oskari.userinterface.extension.DefaultExtension"]
+  });
---
+++ packages/routing/bundle/inapprouting/bundle.js
@@ -0,0 +1,66 @@
+Oskari.clazz.define("Oskari.routing.bundle.inapprouting.InAppRoutingBundle",
+
+ function() {
+
+  }, {
+   "create" : function() {
+      return Oskari.clazz.create("Oskari.routing.bundle.inapprouting.InAppRoutingBundleInstance");
+    },
+   "update" : function(manager, bundle, bi, info) {}
+  },
+ {
+    "protocol" : ["Oskari.bundle.Bundle"],
+    "source" : {
+      "scripts" : [{
+        "type" : "text/javascript",
+        "src" : "../../../../bundles/routing/bundle/inapprouting/instance.js"
+      }],
+      "locales" : [{
+        "lang" : "fi",
+        "type" : "text/javascript",
+        "src" : "../../../../bundles/routing/bundle/inapprouting/locale/fi.js"
+      }, {
+        "lang" : "sv",
+        "type" : "text/javascript",
+        "src" : "../../../../bundles/routing/bundle/inapprouting/locale/sv.js"
+      }, {
+        "lang" : "en",
+        "type" : "text/javascript",
+        "src" : "../../../../bundles/routing/bundle/inapprouting/locale/en.js"
+      }]
+    },
+    "bundle" : {
+      "manifest" : {
+        "Bundle-Identifier" : "inapprouting",
+        "Bundle-Name" : "inapprouting",
+        "Bundle-Author" : [{
+          "Name" : "Tuomas Järvensivu",
+          "Organisation" : "",
+          "Temporal" : {
+            "Start" : "2009",
+            "End" : "2011"
+          },
+          "Copyleft" : {
+            "License" : {
+              "License-Name" : "EUPL",
+              "License-Online-Resource" : ""
+            }
+          }
+        }],
+        "Bundle-Name-Locale" : {
+          "fi" : {
+            "Name" : "InAppRouting",
+            "Title" : "In-App Routing"
+          },
+          "en" : {}
+        },
+        "Bundle-Version" : "1.0.0",
+        "Import-Namespace" : ["Oskari"],
+        "Import-Bundle" : {}
+      }
+    },
+
+   "dependencies" : []
+  });
+
+Oskari.bundle_manager.installBundleClass("inapprouting", "Oskari.routing.bundle.inapprouting.InAppRoutingBundle");
