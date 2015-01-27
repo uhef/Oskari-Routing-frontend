6fe831a602ea78d53ecec9c90da15b2913fff92f
uhef <tuomas.jarvensivu@iki.fi>
2015-01-27 20:20:15 +0200

    Add geometry from route calculation to vector layer

--- bundles/routing/bundle/inapprouting/instance.js
+++ bundles/routing/bundle/inapprouting/instance.js
@@ -30,7 +30,19 @@ Oskari.clazz.define('Oskari.routing.bundle.inapprouting.InAppRoutingBundleInstan
         console.log('Map clicked at ' + event.getLonLat());
         var sandbox = Oskari.getSandbox();
         var url = sandbox.getAjaxUrl() + 'action_route=CalculateRoute';
-        jQuery.getJSON(url, function(data) { console.log('Great success: ', data) });
+        jQuery.ajax({
+          dataType: 'json',
+          url: url,
+          context: this,
+          success: function(data) {
+            var map = this.sandbox._modulesByName.MainMapModule.getMap();
+            var geoJSON = new OpenLayers.Format.GeoJSON();
+            var styleMap = new OpenLayers.StyleMap({ strokeWidth: 16, stokeColor: '#000000' });
+            var layer = new OpenLayers.Layer.Vector("routing", { styleMap: styleMap });
+            map.addLayer(layer);
+            layer.addFeatures(geoJSON.read(data));
+          }
+        });
       }
     },
     /**
