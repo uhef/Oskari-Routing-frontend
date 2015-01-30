282e2a5d16e37040e4c9ef2f5512931ac21aaf84
Tuomas Järvensivu <tuomas.jarvensivu@reaktor.fi>
2015-01-30 18:57:35 +0200

    Remove obsolete code

--- bundles/geojson/bundle/geojson/GeoJSONPlugin.js
+++ bundles/geojson/bundle/geojson/GeoJSONPlugin.js
@@ -1,51 +1,14 @@
 Oskari.clazz.define('Oskari.geojson.bundle.geojson.GeoJSONPlugin',
   function (conf, state) {
-    console.log('constructor called');
     var me = this;
     me._clazz = 'Oskari.geojson.bundle.geojson.GeoJSONPlugin';
     me._name = 'MarkersPlugin';
-
     me.state = state;
-    me.dotForm = null;
-    me._markers = [];
-    me._svg = false;
-    me._defaultIconUrl = '/Oskari/resources/framework/bundle/mapmodule-plugin/images/marker.png';
-    me._defaultIconUrlSize = 32;
-    me._prevIconUrl = '';
-    me._preSVGIconUrl = 'data:image/svg+xml;base64,';
-    me._font = {
-      name: 'dot-markers',
-      baseIndex: 57344
-    };
-    me._defaultData = {
-      x: 0,
-      y: 0,
-      color: 'ffde00',
-      msg: '',
-      shape: 2,
-      size: 1
-    };
-    me._strokeStyle = {
-      'stroke-width': 1,
-      'stroke': '#b4b4b4'
-    };
-    me.buttonGroup = 'selectiontools';
-    me.buttons = null;
-    me.dialog = null;
-    me._buttonsAdded = false;
-
-    // Show the marker button
-    me._markerButton = true;
-    if ((conf) && (typeof conf.markerButton === "boolean")) {
-      me._markerButton = conf.markerButton;
-    }
   }, {
     hasUI: function () {
-      console.log('hasUI called');
       return true;
     },
     _createEventHandlers: function () {
-      console.log('_createEventHandlers called');
       var me = this;
 
       return {
@@ -55,7 +18,6 @@ Oskari.clazz.define('Oskari.geojson.bundle.geojson.GeoJSONPlugin',
       };
     },
     _createRequestHandlers: function () {
-      console.log('_createRequestHandlers called');
       var me = this;
       return {
         'GeoJSONPlugin.AddGeoJSONRequest': Oskari.clazz.create(
@@ -69,34 +31,28 @@ Oskari.clazz.define('Oskari.geojson.bundle.geojson.GeoJSONPlugin',
       };
     },
     register: function () {
-      console.log('register called');
       this.getMapModule().setLayerPlugin('geojson', this);
     },
     unregister: function () {
-      console.log('unregister called');
       this.getMapModule().setLayerPlugin('geojson', null);
     },
     _startPluginImpl: function () {
-      console.log('_startPluginImp called');
       var me = this;
       me._createGeoJSONLayer();
     },
     _createGeoJSONLayer: function () {
-      console.log('_createGeoJSONLayer called');
       var styleMap = new OpenLayers.StyleMap({strokeWidth: 16, stokeColor: '#000000'});
       var layer = new OpenLayers.Layer.Vector('GeoJSON', {styleMap: styleMap});
       this.getMap().addLayer(layer);
       this.raiseLayer(layer);
     },
     addMapLayerToMap: function () {
-      console.log('addMapLayerToMap called');
       var me = this;
       return function () {
         me.raiseLayer();
       };
     },
     raiseLayer: function (geoJSONLayer) {
-      console.log('raiseLayer called');
       var index,
         layer = null;
       if (typeof geoJSONLayer !== 'undefined') {
@@ -111,15 +67,11 @@ Oskari.clazz.define('Oskari.geojson.bundle.geojson.GeoJSONPlugin',
       layer.setZIndex(index);
       layer.setVisibility(true);
     },
-    getState: function () {
-      console.log('getState called');
-    },
-    getOLMapLayers: function (layer) {
-      console.log('getOLMapLayers called');
+    getState: function () {},
+    getOLMapLayers: function () {
       return null;
     },
     addGeoJSON: function(data, groupId) {
-      console.log('**** GeoJSON Data arrived to Geo JSON plugin: ', data);
       var layer = this.getMap().getLayersByName('GeoJSON')[0];
       var geoJSON = new OpenLayers.Format.GeoJSON();
       var features = geoJSON.read(data);
