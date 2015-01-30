21e6b6c6261203d61a3aa4ad85f107d64a36c353
uhef <tuomas.jarvensivu@iki.fi>
2015-01-30 11:58:53 +0200

    Improve routing description

--- bundles/routing/bundle/inapprouting/locale/fi.js
+++ bundles/routing/bundle/inapprouting/locale/fi.js
@@ -3,6 +3,9 @@ Oskari.registerLocalization({
     "key": "inapprouting",
     "value": {
         "title": "Reititys",
-        "description": "Selitys tähän"
+        "description": "Klikkaa karttaa kahdesti: " +
+        "Ensimmäinen klikkaus käynnistää reitityksen klikatuista koordinaateista. " +
+        "Jälkimmäinen klikkaus laskee reitin ensimmäisen ja jälkimmäisen klikkauksen välillä ja piirtää reitin kartalla. " +
+        "Seuraava klikkaus käynnistää uuden reitityksen ja poistaa vanhan reitin kartalta."
     }
 });
--- bundles/routing/bundle/inapprouting/locale/sv.js
+++ bundles/routing/bundle/inapprouting/locale/sv.js
@@ -3,6 +3,9 @@ Oskari.registerLocalization({
     "key": "inapprouting",
     "value": {
         "title": "Routing",
-        "description": "Explanation"
+        "description": "Click the map twice: " +
+        "First click will commence routing from the clicked coordinates. " +
+        "Second click will perform route calculation between first click and second click and show resulting route on the map. " +
+        "Clicking on the map again will remove previous route and commence new route calculation."
     }
 });
